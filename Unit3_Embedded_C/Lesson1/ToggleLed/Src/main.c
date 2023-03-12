14d613fae2ac4ebad35a200d7f63e020eaa2a925
AhmedEssam17 <ahmedessam020@gmail.com>
2023-03-12 18:54:29 +0200

    Revert "Lesson3 - Lab2"
    This reverts commit 01f04302a078d70f10acf7fdf51a60bd280eef83.

--- Unit3_Embedded_C/Lesson1/ToggleLed/Src/main.c
+++ Unit3_Embedded_C/Lesson1/ToggleLed/Src/main.c
@@ -21,24 +21,24 @@
 
 #define RCC_BASE 	0x40021000
 #define PortA_BASE 	0x40010800
+
 #define RCC_APB2ENR *(volatile uint32_t *) (RCC_BASE + 0x18)
 #define GPIOA_CRH 	*(volatile uint32_t *) (PortA_BASE + 0x04)
 #define GPIOA_ODR 	*(volatile uint32_t *) (PortA_BASE + 0x0C)
-#define RCC_IOPAEN 	(1<<2)
 
 typedef volatile unsigned int vuint32_t;
 
 typedef union{
 	vuint32_t allFields;
 	struct{
-		vuint32_t reserved:13; //0-12
+		vuint32_t reserved:13;
 		vuint32_t pin13:1;
 	} pin;
 } R_ODR_t;
 
 volatile R_ODR_t* R_ODR = (volatile R_ODR_t*)(PortA_BASE + 0x0C);
-unsigned char g_var[3] = {1, 2, 3};
-unsigned char const const_var[3] = {1, 2, 3};
+
+#define RCC_IOPAEN (1<<2)
 
 int main(void)
 {
